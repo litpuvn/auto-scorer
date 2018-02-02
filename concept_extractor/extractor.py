0e45740f3784c7568020d255c7efa4a982174020
3dchristopher <crlittleman1@yahoo.com>
2018-02-01 18:58:12 -0600

    Added limited affirmative transformation.
    Unable to transform nouns and verbs. Might be a limitation of nltk.

--- concept_extractor/extractor.py
+++ concept_extractor/extractor.py
@@ -3,8 +3,10 @@
 import string
 import sys
 import os
+import csv
 from pycorenlp import StanfordCoreNLP
 from nltk.corpus import stopwords
+from nltk.corpus import wordnet as wn
 
 nlp = StanfordCoreNLP('http://localhost:9000')
 
@@ -13,9 +15,34 @@ sys.argv.pop(0)
 with open('custom_stopwords.txt') as e:
     stopwordList=e.read()+','.join(set(stopwords.words('english')))
 
-#stopwordList=list(set(stopwordList)).lower()
-#word.lower() for word in stopwordList
+def oppositeDictionary(negT, outputDep): #Generates the list of words that can recieve affirmative transformation. (Does not work on nouns or verbs)
+    for q in outputDep['sentences']:
+        for y in q['basicDependencies']:
+            if y['dep']=="neg":
+                negWord = wn.synsets(y['governorGloss'])
+                nW="1"
+                for synset in negWord:
+                    if str(synset).split(".")[1].split(".")[0] =='a':
+                        nW=str(synset).split("'")[1].split("'")[0]
+                        break
+                if nW!="1":
+                    nW= wn.synset(nW)
+                    negT.update({str(y['governorGloss']):str(nW.lemmas()[0].antonyms()).split("'")[1].split(".")[0]})
+    negT.update({'go':'stay'})
 
+def negCheck(negT, string):
+    negWord = wn.synsets(string)
+    nW="1"
+    val=True
+    for synset in negWord:
+        if str(synset).split(".")[1].split(".")[0] =='a':
+            nW=str(synset).split("'")[1].split("'")[0]
+            break
+    if nW!="1":
+        val=False
+    return val
+
+negT = {}
 for file in sys.argv:
     clname = file[:-4] + "_concepts.txt"
     with open(file,'r') as f: #Reads in sentences from answer_set.txt. Removes punctuation, and annotates
@@ -39,31 +66,36 @@ for file in sys.argv:
                     for z in grade:
                         grade = grade.rstrip()
                     punctuationString = "!#$%&'()*,-./:;<=>?@[]^`{|}~"
-                    output = nlp.annotate(input.translate(None, punctuationString), properties={'annotators': 'tokenize,pos,parse,lemma', 'outputFormat': 'json'})
+                    output = nlp.annotate(input.translate(None, punctuationString), properties={'annotators': 'tokenize,depparse,lemma', 'outputFormat': 'json'})
                     notEmpty=False
+                    oppositeDictionary(negT, output)
+
                     for s in output['sentences']:
                         if grade == '':
                             grade='0'
-                        gOutput=str(grade).zfill(3)+" || "
-                        t.write(gOutput)
-                        notEmpty=True
-                        cs = False
+                        roughS=str(grade).zfill(3)+" || "
+
+                        nT = []
+                        for y in s['basicDependencies']: #Collects the list of words in the sentence that need to be negated
+                            if y['dep']=="neg":
+                                nT.append(y['governor'])
+
+                        count=0
                         for w in s["tokens"]:
-                            if w["word"]=='_':
-                                t.write(" | ")
-                                cs=False
-                            elif w["word"]=='+':
-                                t.write("\n")
-                                cs=False
-                            elif w["word"] in stopwordList: #removes stopwords from the input
-                                t.write("")
+                            count=count+1
+                            if count in nT:
+                                if negCheck(negT, w["word"])!=True: #If the word can be negated, and needs to be negated, negate it
+                                    roughS=roughS+negT[w["word"]]+" "
                             else:
-                                if cs==False:
-                                    cs=True
-                                    #t.write("'%s': %s" % (w["word"], w['pos']))
-                                    t.write("%s" % (w["lemma"]))
-                                else:
-                                    #t.write(", '%s': %s" % (w["word"], w['pos']))
-                                    t.write(", %s" % (w["lemma"]))
-                    if notEmpty==False:
-                        t.write('\n')
+                                roughS=roughS+w["lemma"]+" "
+                        if roughS[0:1]==" _":
+                            roughS=roughS[1:]
+                        roughS=roughS.split()
+                        roughS = [word for word in roughS if word not in stopwordList] #Removes stopwords from the line
+                        roughS = ', '.join(roughS)
+                        roughS = roughS.replace('_,','|')
+                        roughS = roughS.replace(', |',' |')
+                        roughS = roughS.replace('||,','||')
+                        roughS = roughS.replace('|| | ','|| ')
+                        t.write(roughS+"\n")
+
